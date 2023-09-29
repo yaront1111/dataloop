a914e6813f9bbd2478b1a93935dcd3519c4d4785
Yaron Torgeman <yaron.torgeman@cyberark.com>
2023-09-29 15:31:49 +0300

    fix vars

--- modules/gcp/networking/vpc.tf
+++ modules/gcp/networking/vpc.tf
@@ -4,9 +4,6 @@ resource "google_compute_network" "vpc" {
   auto_create_subnetworks = var.auto_create_subnetworks
   routing_mode            = var.routing_mode
   description             = var.vpc_description
-}
-resource "google_compute_network_label" "vpc_label" {
-  network = google_compute_network.vpc.name
   labels = {
     environment = var.envtags
   }
