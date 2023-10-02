5e622d4f64282ad637fceb9893f7ea38eebe92f1
Yaron Torgeman <yaron.torgeman@cyberark.com>
2023-10-03 00:48:27 +0300

    Remove account1.json from repository

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,8 @@
   # Initialize Terraform
   cd ../environments/production/company_a/gcp/
-
   terraform init -upgrade
 
   terraform validate
   # Run Terraform plan
-  terraform plan -var-file="company_a.tfvars"
-  terraform apply -var-file="company_a.tfvars"
+  terraform plan -var-file="company_a.tfvars" -lock=false
+  terraform apply -var-file="company_a.tfvars" -lock=false
